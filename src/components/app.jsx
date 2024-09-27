4742ed91bcb1e2610cfbad763ff8f00497f70d28
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-09-27 12:09:39 +0300

    position

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,7 +10,7 @@ import React from 'react';
 export default function App(){
   return(
    
-      <div className='container'>
+      <div >
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Main />} />
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,22 +3,34 @@ import { Link } from 'react-router-dom';
 import Elipse from '../img/Ellipse.png';
 import buttons from '../img/buttons.png';
 
-export const Main = () =>{
-  return(
-    <>
-    {/* <div className="marquee-container">
+export const Main = () => {
+  return (
+    <section className='main-container'>
+      {/* <div className="marquee-container">
   <div className="marquee-row marquee-left">Текст біжить вліво</div>
   <div className="marquee-row marquee-right">Текст біжить вправо</div>
 </div> */}
-    <h1 className='text'>full-cycle event agency</h1>
-    <img className='circle' src={buttons}/>
+      <img className="elipse" src={Elipse}></img>
+      <h1 className="text">FULL-CYCLE<br />EVENT AGENCY</h1>
+      <img className="circle" src={buttons} />
 
-   <img className='elipse' src={Elipse}></img>
-    <ul className='nav-link link'>
-      <li><Link to="where" className='rotated-link1'>Where?</Link></li>
-      <li><Link to="who" className='rotated-link2'>Who?</Link></li>
-      <li><Link to="what" className='rotated-link3'>What?</Link></li>
-    </ul>
-    </>
-  )
-}
+      <ul className="nav-link link">
+        <li>
+          <Link to="where" className="rotated-link1">
+            Where?
+          </Link>
+        </li>
+        <li>
+          <Link to="who" className="rotated-link2">
+            Who?
+          </Link>
+        </li>
+        <li>
+          <Link to="what" className="rotated-link3">
+            What?
+          </Link>
+        </li>
+      </ul>
+    </section>
+  );
+};
