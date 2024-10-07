2b05c3146def8d469ee0c7d691793e47076ef481
MateichukNina <124276133+MateichukNina@users.noreply.github.com>
2024-10-08 02:49:38 +0300

    update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,21 +1,24 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import { NavLink } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import  Group  from '../img/Group 53.png';
 import  Union  from '../img/Union.svg';
 import Burger from '../img/Burger.svg';
+
 export const Layout = () => {
+
+
   return (
     <>
       <div className='header'>
         <ul className='nav-list '>
           <li>
-            <NavLink  ><img src={Group}></img></NavLink>
-            <div className="language-options">
-              <button className="language-item">Eng</button>
-              <button className="language-item">Ua</button>
-            </div>
+            <NavLink><img src={Group}></img></NavLink>
+            <div className="language-options" >
+        <button className="language-item">Eng</button>
+        <button className="language-item">Ua</button>
+      </div>
           </li>
           <li>
             <NavLink className='logo logo-container' to="/"> <img src={Union} /></NavLink>
